f3bc28313141164479ac6803e14643d105b4d063
Steven Penny <srpen6@gmail.com>
2021-08-11 16:31:38 -0500

    new file:   manifest.json

---
+++ popup.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+browser.runtime.onMessage.addListener(message => {
+   let h1 = document.createElement('h1');
+   h1.textContent = message.greeting;
+   document.body.appendChild(h1);
+});
+*/
+
+console.log('popup');
---
+++ tab.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+document.body.onclick = function() {
+   browser.runtime.sendMessage({
+      greeting: "Greeting from the tab script"
+   });
+};
+*/
+
+console.log('tab');
