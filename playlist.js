5328d240f33b1c087bea670b1438abec1447db56
Steven Penny <srpen6@gmail.com>
2021-08-12 07:32:53 -0500

    +   <video id="video" controls></video>

--- playlist.js
+++ playlist.js
@@ -1,16 +1,37 @@
 'use strict';
 
-browser.runtime.onMessage.addListener(play => {
-   let vd = play.videoDetails;
+function append(play) {
+   let fig = temp.content.cloneNode(true);
    // author
-   document.getElementById('author').textContent = vd.author;
+   fig.querySelector('.author').textContent = play.videoDetails.author;
    // title
-   document.getElementById('title').textContent = vd.title;
-   // audio
+   fig.querySelector('.title').textContent = play.videoDetails.title;
+   // poster
+   let vid = fig.querySelector('video');
+   vid.poster = play.videoDetails.thumbnail.thumbnails[1].url;
+   // src
    for (let fmt of play.streamingData.adaptiveFormats) {
       if (fmt.mimeType.startsWith('audio/webm;')) {
-         document.getElementById('audio').src = fmt.url;
+         vid.src = fmt.url;
          break;
       }
    }
-});
+   // ended
+   vid.onended = next;
+   // append
+   main.append(fig);
+}
+
+function next() {
+   // remove
+   main.querySelector('figure').remove();
+   // play
+   let vid = main.querySelector('video');
+   if (vid !== null) {
+      vid.play();
+   }
+}
+
+let main = document.querySelector('main');
+let temp = document.querySelector('template');
+browser.runtime.onMessage.addListener(append);
